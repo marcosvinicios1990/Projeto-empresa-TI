7ef87f8a2e94be9345a8bd0fe6ac9536b7d632eb
MarcosVinicios1990 <mvinicios1990@gmail.com>
2021-02-04 22:22:47 -0300

    Conexão no banco de dados MySQL

--- cadastro.php
+++ cadastro.php
@@ -1,18 +1,6 @@
 <?php
 
-$host = "mysql:host=localhost;dbname=site_ti;port=3306";
-$user = "root";
-$pass = "";
-
-try{
-$db = new PDO($host,$user,$pass);
-/*echo "Conexão feita com sucesso!";*/
-}catch(PDOException $e){
-/*echo "Não foi possivel realizar a conexão do banco de dados";*/
-exit;
-}
-
-
+include('pdoconexao.php')
 
 
 ?>
--- salvacadastro.php
+++ salvacadastro.php
@@ -1,5 +1,23 @@
 <?php
+    session_start();
+    include "pdoconexao.php";
+  
+  
+  $query = $db->prepare("INSERT INTO cadastro_clientes
+  (nome,email,cpf,celular,genero,senha)
+  VALUES ('{$_POST['nome']}',
+          '{$_POST['email']}',
+          '{$_POST['cpf']}',
+          '{$_POST['celular']}',
+          '{$_POST['genero']}',
+          '{$_POST['senha']}')
+          ");
 
-    echo var_dump($_POST);
+//$query->execute();
 
-?>
+if ($query->execute()){
+    header("Location: produtos.php"); //Se enviar corretamente redireciona para página de produtos
+}else{
+    $_SESSION['msg'] = "<p style='color:tomato;background:#fff;'>Não foi possível enviar suas informações, verifique e tente novamente.</p>";
+    header("Location:cadastro.php"); //Se não apresenta o erro
+}
